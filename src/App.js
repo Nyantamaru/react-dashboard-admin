13c83b95fc3e8011cb2456b796905a0351e88025
Manuel <manekete@gmail.com>
2022-10-20 20:47:43 -0600

    1 dark mode template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import './App.css';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => <div className="App">My super cool dashboar App.</div>;
+import Topbar from './scenes/global/Topbar';
+import Dashboard from './scenes/dashboard';
+
+const App = () => {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
 
 export default App;
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashbard = () => {
+  return <div>My super cool app Dashbard</div>;
+};
+
+export default Dashbard;
